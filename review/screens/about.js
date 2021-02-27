646cba2e308126c7584145aaf234876dbf5813c7
ashwath-p <ashwathp.2019@vitstudent.ac.in>
2021-02-27 19:25:45 +0530

    globalStyle
    -created a styles folder
    -created globalStyles
    -deleted local styles

--- review/screens/about.js
+++ review/screens/about.js
@@ -1,16 +1,12 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { globalStyles } from '../styles/global';
+
 
 export default function About() {
     return (
-        <View style={styles.container}>
+        <View style={globalStyles.container}>
             <Text>About Page</Text>
         </View>
     )
 }
-
-const styles = StyleSheet.create({
-    container: {
-        padding: 24
-    },
-});
--- review/screens/home.js
+++ review/screens/home.js
@@ -1,19 +1,11 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { globalStyles } from '../styles/global';
 
 export default function Home() {
     return (
-        <View style={styles.container}>
-            <Text style={styles.text}>Home</Text>
+        <View style={globalStyles.container}>
+            <Text style={globalStyles.text}>Home</Text>
         </View>
     )
 }
-
-const styles = StyleSheet.create({
-    container: {
-        padding: 24
-    }, text: {
-        fontFamily: 'futura-book',
-        fontSize: 25,
-    },
-});
