941e85c8148a905ef01efc9ef2dc8f89776e6c63
ashwath-p <ashwathp.2019@vitstudent.ac.in>
2021-03-11 06:35:11 +0530

    Navigation 2.0
    Navigating around screens

--- review/screens/home.js
+++ review/screens/home.js
@@ -1,11 +1,17 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import { globalStyles } from '../styles/global';
 
-export default function Home() {
+export default function Home({ navigation }) {
+
+    const pressHandler = () => {
+        navigation.navigate('ReviewDetails');
+    };
+
     return (
         <View style={globalStyles.container}>
             <Text style={globalStyles.text}>Home</Text>
+            <Button title='Review Details' onPress={pressHandler} />
         </View>
     )
 }
