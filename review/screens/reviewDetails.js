d3847cb2ae1a5ee45582fdf8e7e01972dd9f68bb
ashwath-p <ashwathp.2019@vitstudent.ac.in>
2021-03-11 22:15:41 +0530

    Dynamic Images
    - Added dynamic images
    - Imported from global styles

--- review/screens/reviewDetails.js
+++ review/screens/reviewDetails.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import { globalStyles } from '../styles/global';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { globalStyles, images } from '../styles/global';
 import Card from '../shared/card';
 
 export default function ReviewDetails({ navigation }) {
@@ -9,14 +9,29 @@ export default function ReviewDetails({ navigation }) {
         navigation.goBack();
     }
 
+    const rating = navigation.getParam('rating');
+
     return (
         <View style={globalStyles.container}>
             <Card>
                 <Text style={globalStyles.titleText}>{navigation.getParam('title')}</Text>
-                <Text style={globalStyles.titleText}>{navigation.getParam('rating')}</Text>
                 <Text style={globalStyles.titleText}>{navigation.getParam('body')}</Text>
-                <Button title='goback' onPress={navg} />
+                <View style={styles.rating}>
+                    <Text>Rating is: </Text>
+                    <Image source={images.ratings[rating]} />
+                </View>
             </Card>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    rating: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        paddingTop: 16,
+        marginTop: 16,
+        borderTopWidth: 1,
+        borderTopColor: '#eee',
+    }
+});
