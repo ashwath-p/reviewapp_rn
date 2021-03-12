6705e152fd841824de87cb89f36cae24c2c58656
ashwath-p <ashwathp.2019@vitstudent.ac.in>
2021-03-12 17:32:23 +0530

    Yup 2.0
    - Upgraded the form to display real time errors
    - Styled the errors displayed

--- review/screens/reviewForms.js
+++ review/screens/reviewForms.js
@@ -23,9 +23,9 @@ export default function ReviewForm({ addReview }) {
             <Formik
                 initialValues={{ title: '', body: '', rating: '' }}
                 validationSchema={reviewSchema}
-                onSubmit={(values) => {
-                    addReview(values);
+                onSubmit={(values, actions) => {
                     actions.resetForm();
+                    addReview(values);
                 }}
             >
                 {props => (
@@ -35,24 +35,27 @@ export default function ReviewForm({ addReview }) {
                             placeholder='Review title'
                             onChangeText={props.handleChange('title')}
                             value={props.values.title}
+                            onBlur={props.handleBlur('title')}
                         />
-
+                        <Text style={globalStyles.errorText}>{props.touched.title && props.errors.title}</Text>
                         <TextInput
                             style={globalStyles.input}
                             multiline
                             placeholder='Review details'
                             onChangeText={props.handleChange('body')}
                             value={props.values.body}
+                            onBlur={props.handleBlur('body')}
                         />
-
+                        <Text style={globalStyles.errorText}>{props.touched.body && props.errors.body}</Text>
                         <TextInput
                             style={globalStyles.input}
                             placeholder='Rating (1 - 5)'
                             onChangeText={props.handleChange('rating')}
                             value={props.values.rating}
                             keyboardType='numeric'
+                            onBlur={props.handleBlur('rating')}
                         />
-
+                        <Text style={globalStyles.errorText}>{props.touched.rating && props.errors.rating}</Text>
                         <Button color='maroon' title="Submit" onPress={props.handleSubmit} />
                     </View>
                 )}
